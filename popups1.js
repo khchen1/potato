264e9da147754cd8502c3633d9a954664b24e724
George Chan <georgechan32@gmail.com>
2014-04-12 09:55:16 -0700

    Hellyea?

---
+++ popups1.js
@@ -0,0 +1,10 @@
+function show_image(src, width, height, alt) {
+    var img = document.createElement("img");
+    img.src = src;
+    img.width = width;
+    img.height = height;
+    img.alt = alt;
+    document.body.appendChild(img);
+}
+
+show_image("harris.jpg", 500, 500, 'alsdkf');
