8d1fb59a4743db461040ae2c887f00f00dd8df22
George Chan <georgechan32@gmail.com>
2014-04-12 09:54:23 -0700

    no message

--- popups.js
+++ popups.js
@@ -1,44 +1,32 @@
-var time= "George is fucking awesome";
-var imgScreen = new Image();
-imgScreen.src = 'harris.jpg';
-function myFunction()
-{
-	document.write(time);
-//	time = time - 1;
-}
-
-function show_image(src, width, height, alt)
-{
-	var img = document.createElement("img");
-	img.src = src;
-	img.width = width;
-	img.height = height;
-	img.alt = alt;
-	
-	document.body.appendChild(img);
-}
-
-
-
-// /*
-//Run our kitten generation script as soon as the document's DOM is ready.
-// document.addEventListener('DOMContentLoaded', function () {
-	// myFunction();
-// });
-// */
-
-// while(time!=0)
-// {
-	// window.setInterval(myFunction(),1000);
-// }
+// Called when the user clicks on the browser action.
+var blackList = true;
 
 // chrome.browserAction.onClicked.addListener(function(tab) {
+//  No tabs or host permissions needed!
   // console.log('Turning ' + tab.url + ' red!');
-  // chrome.tabs.executeScript({
-    // code: 'document.body.style.backgroundColor="red"'
-  // });
+// window.location.href = "google.com";
+  // while(blackList == true)
+  // {
+	// if(tab.url != "www.youtube.com"){
+	// chrome.tabs.executeScript({
+    // code: 'location.replace("http://www.google.com");'
+  // });}
+  // }
 // });
 
-myFunction();
-onClick = show_image('harris.jpg',150, 200, 'harry');
-//ThankYouQuixey
+
+
+function click(e) {
+	while(blackList == true){
+		if(window.location.href != "www.youtube.com"){
+			chrome.tabs.executeScript({code: 'location.replace("http://www.google.com");'});
+		}
+	}
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  var divs = document.querySelectorAll('div');
+  for (var i = 0; i < divs.length; i++) {
+    divs[i].addEventListener('click', click);
+  }
+});
